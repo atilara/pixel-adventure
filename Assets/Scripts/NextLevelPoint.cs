41df25da5287e5ed437ea53a6dd6faaf90463230
Átila Rodrigues <atila.rodriguesal@gmail.com>
2024-04-27 18:11:31 -0300

    Add trophy to finish level and go to the next

---
+++ Assets/Scripts/NextLevelPoint.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevelPoint : MonoBehaviour
+{
+    private Animator animator;
+
+    public string nextLevel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        animator.SetBool("level_ended", false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        animator.SetBool("level_ended", true);
+        Invoke("NextLevel", 1f);
+    }
+
+    void NextLevel()
+    {
+        SceneManager.LoadScene(nextLevel);
+    }
+}
